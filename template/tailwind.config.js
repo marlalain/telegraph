d0086443426124782255e45549d54e70d6a0d344
Paulo Elienay <paulo@pauloelienay.com>
2021-07-13 05:24:17 -0300

    style(template): adds template

---
+++ template/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	purge: [],
+	darkMode: "media", // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					100: "#F1F1F1",
+					200: "#9D9EA0",
+					300: "#C0A097",
+					400: "#9B836A",
+					800: "#8A644D",
+					900: "#1F1D1D",
+				},
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
