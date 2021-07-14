df17fa4cc1db05a6c87dd82abf7127eabad5902f
Paulo Elienay <paulo@pauloelienay.com>
2021-07-14 09:55:31 -0300

    feat(blog): adds Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from .models import Link
+from .models import Link, Post
 
 # Register your models here.
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author_name', 'created_on')
+    search_fields = ['title', 'content']
+
+
 admin.site.register(Link)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,19 @@ class Link(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    # `<a>{}</a>` in the author section
+    author_name = models.CharField(max_length=50)
+    # `<a href="{}">` in the author section
+    author_link = models.CharField(max_length=255)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
