e761e85b905fab135985b582e5415b1ebf33eaeb
Paulo Elienay <paulo@pauloelienay.com>
2021-07-14 10:21:24 -0300

    feat(blog): implements Post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Link, Post
 
 # Create your views here.
+
+
+class NavBarView(generic.DetailView):
+    model = Link
+    template_name = "blog/navbar.html"
+
+
+class PostsView(generic.ListView):
+    queryset = Post.objects.order_by('-created_on')
+    template_name = "blog/layout.html"
