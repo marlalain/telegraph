017a79cbecc044520741ce37248ad4d1f00a4434
Paulo Elienay <paulo@pauloelienay.com>
2021-07-14 09:31:47 -0300

    feat: implements Links

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Link(models.Model):
+    name = models.CharField(max_length=50)
+    shortname = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
